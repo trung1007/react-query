9d4bbc38d3f4f3309b25a94c8d84d523643ca9c9
trung1007 <trungthanhcao.2003@gmail.com>
2025-04-03 17:00:58 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './components/header';
+import TabsContent from './components/tabs.content';
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <TabsContent />
+    </>
+  )
+}
+
+export default App
